a9be723eae90c41d53ad6eee6786e8304bbaf4bc
Frederick Corpuz <fcorpuz@wesleyan.edu>
2019-12-11 11:00:18 -0500

    Get notes/descriptions for vars w/ tests

--- R/utils.r
+++ R/utils.r
@@ -1,7 +1,38 @@
-get_description <- function(.dataset, .col_id){
+#' @import dplyr
+#' @export
+get_description <- function(search_result, col_id){
+  description_df <- search_result %>%
+    rename(var = col_id) %>%
+    filter(var == col_id)
+
+  n <- nrow(description_df)
+  if(n == 0){
+    stop(paste("The variable", col_id, "is not found in your search results"))
+  } else if(n == 1){
+    pull(description_df, description)
+  } else {
+    stop(
+      paste("The variable", col_id, "is not distinct in your search results")
+    )
+  }
 
 }
 
-get_notes <- function(.dataset, .col_id){
+#' @import dplyr
+#' @export
+get_note <- function(search_result, col_id){
+  note_df <- search_result %>%
+    rename(var = col_id) %>%
+    filter(var == col_id)
 
+  n <- nrow(note_df)
+  if(n == 0){
+    stop(paste("The variable", col_id, "is not found in your search results"))
+  } else if(n == 1){
+    pull(note_df, notes)[[1]]
+  } else {
+    stop(
+      paste("The variable", col_id, "is not distinct in your search results")
+    )
+  }
 }

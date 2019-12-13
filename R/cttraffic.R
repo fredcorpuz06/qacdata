ba5f5d7fad203fb98cb299a381f1a4fc922f18ec
Frederick Corpuz <fcorpuz@wesleyan.edu>
2019-12-13 00:05:54 -0500

    Fix typo in documentation + update man files

--- R/cttraffic.R
+++ R/cttraffic.R
@@ -1,7 +1,8 @@
 #' @title CtTraffic
-#' @description Connecticut Racial Profiling Prohibition Project Data 
+#' @description Connecticut Racial Profiling Prohibition Project Data
 #' @docType data
 #' @keywords datasets
 #' @name CTTraffic
-#' @usage CtTraffhttps://data.ct.gov/Public-Safety/Traffic-Stops-Racial-Profiling-Prohibition-Project/nahi-zqrt}{CTData.gov}
+#' @usage CtTraffic
+#' @source \href{https://data.ct.gov/Public-Safety/Traffic-Stops-Racial-Profiling-Prohibition-Project/nahi-zqrt}{CTData.gov}
 NULL

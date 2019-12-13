95a0b0e7afae7f5f01e50bac212a86224821bebb
kkkoech <kkoech@wesleyan.edu>
2019-12-12 23:49:59 -0500

    documentation for datasets

---
+++ R/cttraffic.R
@@ -0,0 +1,7 @@
+#' @title CtTraffic
+#' @description Connecticut Racial Profiling Prohibition Project Data 
+#' @docType data
+#' @keywords datasets
+#' @name CTTraffic
+#' @usage CtTraffhttps://data.ct.gov/Public-Safety/Traffic-Stops-Racial-Profiling-Prohibition-Project/nahi-zqrt}{CTData.gov}
+NULL
---
+++ R/marscrater.R
@@ -0,0 +1,7 @@
+#' @title MarsCrater
+#' @description Global database for Mars that contains 378,540 craters statistically complete for diameters D ≥ 1 km
+#' @keywords datasets
+#' @name MarsCrater
+#' @usage MarsCrater
+#' @source \href{https://ieee-dataport.org/open-access/mars-crater-segmentation-dataset}{IEEEDataPort}
+NULL
